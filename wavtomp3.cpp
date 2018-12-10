13c349523f502e82ee3d8f3b3252ddf3872deb05
ajal <ajalvaruthunny@gmail.com>
2018-12-11 00:38:35 +0100

    trial 1

--- wavtomp3.cpp
+++ wavtomp3.cpp
@@ -1,36 +1,73 @@
 #include <stdio.h>
 #include <lame/lame.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <dirent.h>
 
-int main(void)
+
+using namespace std;
+
+bool has_suffix(const string& s, const string& suffix)
+{
+    return (s.size() >= suffix.size()) && equal(suffix.rbegin(), suffix.rend(), s.rbegin());    
+}
+
+int main(int argc, char **argv)
 {
     int read, write;
+    cout << "Directory name: ";
+    string path;
+    getline(cin, path);
 
-    FILE *pcm = fopen("file.pcm", "rb");
-    FILE *mp3 = fopen("file.mp3", "wb");
+    DIR *dir = opendir(path.c_str());
+    if(!dir)
+    {
+        return 1;
+    }
 
-    const int PCM_SIZE = 8192;
-    const int MP3_SIZE = 8192;
+    dirent *entry;
+    while((entry = readdir(dir))!=null) 
+    {
+        if(has_suffix(entry->d_name, ".wav"))
+        {
+            cout << entry->d_name << endl;
 
-    short int pcm_buffer[PCM_SIZE*2];
-    unsigned char mp3_buffer[MP3_SIZE];
+                FILE *pcm = fopen(path + /entry->d_name +".wav", "rb");
+                FILE *mp3 = fopen(path + /entry->d_name +".mp3", "wb");
 
-    lame_t lame = lame_init();
-    lame_set_in_samplerate(lame, 44100);
-    lame_set_VBR(lame, vbr_default);
-    lame_init_params(lame);
+                const int PCM_SIZE = 8192;
+                const int MP3_SIZE = 8192;
 
-    do {
-        read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
-        if (read == 0)
-            write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
-        else
-            write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
-        fwrite(mp3_buffer, write, 1, mp3);
-    } while (read != 0);
+                short int pcm_buffer[PCM_SIZE*2];
+                unsigned char mp3_buffer[MP3_SIZE];
 
-    lame_close(lame);
-    fclose(mp3);
-    fclose(pcm);
+                lame_t lame = lame_init();
+                lame_set_in_samplerate(lame, 44100);
+                lame_set_VBR(lame, vbr_default);
+                lame_init_params(lame);
 
-    return 0;
+                do {
+                    read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
+                    if (read == 0)
+                        write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
+                    else
+                        write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+                    fwrite(mp3_buffer, write, 1, mp3);
+                } while (read != 0);
+
+                lame_close(lame);
+                fclose(mp3);
+                fclose(pcm);
+
+                    }
+                }
+
+    closedir(dir);
 }
+
+
+
+    
+
+    
