735c8b0d763ea2f433e6174a96372f698b4c6f51
ajal <ajalvaruthunny@gmail.com>
2018-12-11 00:19:00 +0100

    intial

---
+++ CMakeFiles/akshay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/akshay.dir/wavtomp3.cpp.o"
+  "akshay.pdb"
+  "akshay"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/akshay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
