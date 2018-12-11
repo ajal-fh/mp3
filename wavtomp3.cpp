083e7489fae43821123384db8271ce24bf78a098
ajal <ajalvaruthunny@gmail.com>
2018-12-11 01:54:59 +0100

    seg fault

--- wavtomp3.cpp
+++ wavtomp3.cpp
@@ -21,6 +21,9 @@ int main(int argc, char **argv)
     int read, write;
     cout << "Directory name: ";
     string path,strOldfile,strNewfile;
+    
+    const char * oldF = strOldfile.c_str();
+    const char * newF = strNewfile.c_str();
     getline(cin, path);
 
     DIR *dir = opendir(path.c_str());
@@ -41,8 +44,10 @@ int main(int argc, char **argv)
                 strNewfile = path + "/" + (fName).substr(0,fileLen - 4) + ".mp3";
                 cout << strOldfile << endl;
                 cout << strNewfile << endl;
-                FILE *pcm = fopen(strOldfile, "rb");
-                FILE *mp3 = fopen(strNewfile, "wb");
+
+
+                FILE *pcm = fopen(oldF, "rb");
+                FILE *mp3 = fopen(newF, "wb");
 
                 const int PCM_SIZE = 8192;
                 const int MP3_SIZE = 8192;
@@ -56,12 +61,14 @@ int main(int argc, char **argv)
                 lame_init_params(lame);
 
                 do {
+                    
                     read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
                     if (read == 0)
                         write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
                     else
                         write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
                     fwrite(mp3_buffer, write, 1, mp3);
+                    cout<<"after fwrite";
                 } while (read != 0);
 
                 lame_close(lame);
